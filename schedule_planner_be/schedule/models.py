ffb090d52614f64787427942629a41064f20aa0b
GatsuraOS <liolic_88@mail.ru>
2022-08-05 03:09:11 +0300

    change model Location

--- schedule_planner_be/schedule/models.py
+++ schedule_planner_be/schedule/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class SubwayStation(models.Model):
-    station = models.CharField(max_length=50)
+    station = models.CharField(max_length=50, default=None)
 
 
 class Location(models.Model):
