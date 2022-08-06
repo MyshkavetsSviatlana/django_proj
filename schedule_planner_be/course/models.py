49c17fe5f87113eaf1559ccfda4d7bd783d03595
Ангелина <a-hanevich@yandex.ru>
2022-08-06 20:07:19 +0300

    add model schedule and views in course

--- schedule_planner_be/course/models.py
+++ schedule_planner_be/course/models.py
@@ -1,19 +1,13 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.utils.datetime_safe import date
-from django.core.exceptions import ValidationError
-
-
-def validate_number_of_lessons(value):
-    """"Проверить кол-во занятий по курсу. Число должно быть натуральным."""
-    if value < 0:
-        raise ValidationError("Number of lessons should be more than 0")
 
 
 class Course(models.Model):
     """Создание модели Course"""
-    course_name = models.CharField(max_length=50)
-    teacher = models.CharField(max_length=50)
+    course_name = models.CharField("Название курса", max_length=50)
+    teacher = models.OneToOneField('Teacher.Teacher', on_delete=models.DO_NOTHING, primary_key=True)
     start_day = models.DateField(default=date.today)
     DAY_OF_WEEK = [
         ("Monday", "Monday"),
@@ -24,25 +18,46 @@ class Course(models.Model):
         ("Saturday", "Saturday"),
         ("Sunday", "Sunday"),
     ]
-    day_of_week = models.CharField(
-        choices=DAY_OF_WEEK,
-        default=None,
-        max_length=50
-    )
-    time = models.TimeField(default=timezone.now)
-    location = models.CharField(max_length=50)
-    classroom = models.CharField(max_length=50)
-    number_of_lessons = models.IntegerField(validators=[validate_number_of_lessons])
+    day_of_week = models.CharField("День недели",
+                                   choices=DAY_OF_WEEK,
+                                   default=None,
+                                   max_length=50
+                                   )
+    time = models.TimeField("Время", default=timezone.now)
+    location = models.ForeignKey('schedule.Location', on_delete=models.DO_NOTHING)
+    classroom = models.CharField("Аудитория", max_length=50)
+    number_of_lessons = models.PositiveSmallIntegerField("Кол-во уроков", default=0)
     COURSE_TYPE = [
         ("Evening schedule", "Evening schedule"),
         ("Morning schedule", "Morning schedule"),
     ]
-    course_type = models.CharField(
-        choices=COURSE_TYPE,
-        default=None,
-        max_length=50
-    )
-    second_teacher = models.BooleanField()
+    course_type = models.CharField("Вид курса",
+                                   choices=COURSE_TYPE,
+                                   default=None,
+                                   max_length=50
+                                   )
+    second_teacher = models.BooleanField("Второй преподаватель")
+    url = models.SlugField(max_length=160, unique=True, default=None)
 
     def __str__(self):
         return f"{self.course_name}, {self.start_day}"
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Comment(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey('User.User', on_delete=models.DO_NOTHING)
+    body = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "К"
+        verbose_name_plural = "Курсы"
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.user, self.course)
