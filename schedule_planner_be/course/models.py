da58baa6c9fb329d924db9d05c9cab0074344fc9
MyshkavetsSviatlana <myshkavets.sviatlana@gmail.com>
2022-08-11 16:31:44 +0300

    comment view

--- schedule_planner_be/course/models.py
+++ schedule_planner_be/course/models.py
@@ -3,11 +3,14 @@ from django.urls import reverse
 from django.utils import timezone
 from django.utils.datetime_safe import date
 
+from Teacher.models import Teacher
+from User.models import User
+
 
 class Course(models.Model):
     """Создание модели Course"""
     course_name = models.CharField("Название курса", max_length=50)
-    teacher = models.OneToOneField('Teacher.Teacher', on_delete=models.DO_NOTHING, primary_key=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
     start_day = models.DateField(default=date.today)
     DAY_OF_WEEK = [
         ("Monday", "Monday"),
@@ -24,7 +27,7 @@ class Course(models.Model):
                                    max_length=50
                                    )
     time = models.TimeField("Время", default=timezone.now)
-    location = models.ForeignKey('schedule.Location', on_delete=models.DO_NOTHING)
+    location = models.ForeignKey("schedule.Location", on_delete=models.DO_NOTHING)
     classroom = models.CharField("Аудитория", max_length=50)
     number_of_lessons = models.PositiveSmallIntegerField("Кол-во уроков", default=0)
     COURSE_TYPE = [
@@ -37,13 +40,13 @@ class Course(models.Model):
                                    max_length=50
                                    )
     second_teacher = models.BooleanField("Второй преподаватель")
-    url = models.SlugField(max_length=160, unique=True, default=None)
+    url = models.URLField(max_length=160, unique=True, default=None)
 
     def __str__(self):
         return f"{self.course_name}, {self.start_day}"
 
     def get_absolute_url(self):
-        return reverse('course_detail',kwargs={'slug': self.url})
+        return reverse('course_detail', kwargs={'slug': self.url})
 
     class Meta:
         verbose_name = "Курс"
@@ -53,7 +56,7 @@ class Course(models.Model):
 class Comment(models.Model):
     """Создание модели Комментарий"""
     course = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
-    user = models.ForeignKey('User.User', on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     body = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
@@ -63,4 +66,4 @@ class Comment(models.Model):
         verbose_name_plural = "Комментарии"
 
     def __str__(self):
-        return f'Comment by {self.user} on {self.course}'
+        return f'{self.body}'
--- schedule_planner_be/schedule/models.py
+++ schedule_planner_be/schedule/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+from course.models import Course, Comment
+
 
 class SubwayStation(models.Model):
     """Создание модели станций метро"""
     station = models.CharField(max_length=50, default=None)
+
+    def __str__(self):
+        return f"Subway station {self.station}"
 
     class Meta:
         verbose_name = 'Станция метро'
@@ -29,10 +34,10 @@ class Schedule(models.Model):
     """Создание модели расписания"""
 
     title = models.TextField(default=None)
-    courses = models.ManyToManyField('course.Course', verbose_name='Курсы')
-    locations = models.ManyToManyField(Location, verbose_name='Локации')
-    reviews = models.ManyToManyField('course.Comment', verbose_name='Комментарии')
-    url = models.SlugField(max_length=160, unique=True, default=None)
+    courses = models.ForeignKey(Course, on_delete=models.DO_NOTHING, verbose_name='Курсы')
+    locations = models.ForeignKey(Location, on_delete=models.DO_NOTHING, verbose_name='Локации')
+    reviews = models.ForeignKey(Comment, on_delete=models.DO_NOTHING, verbose_name='Комментарии')
+    url = models.URLField(max_length=160, unique=True, default=None)
 
     def __str__(self):
         return f'{self.title}'
