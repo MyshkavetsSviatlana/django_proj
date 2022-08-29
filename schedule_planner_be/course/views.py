093a150764ae8d48e20db534d31f54254e496363
Ангелина <a-hanevich@yandex.ru>
2022-08-29 23:41:28 +0300

    refactor LessonView

--- schedule_planner_be/course/views.py
+++ schedule_planner_be/course/views.py
@@ -1,6 +1,7 @@
+import csv
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
-from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Course, Comment, Lesson
 from Teacher.models import Teacher
 from django.views.generic import ListView, DetailView
@@ -208,3 +209,41 @@ class FilterEveningLessonView(LoginRequiredMixin, GetValuesFoFilters, ListView):
             )
         return queryset
 
+
+def csv_courses_list_write(request):
+    """""Create a CSV file with teachers list"""
+    # Get all data from Teacher Database Table
+    courses = Course.objects.all()
+
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="courses_list.csv"'
+    response.write(u'\ufeff'.encode('utf8'))
+    writer = csv.writer(response, delimiter=';', dialect='excel')
+    writer.writerow(['id', 'Название курса', 'Преподаватель', 'Дата старта', 'Время начала', 'Кол-во уроков'])
+
+    for course in courses:
+        writer.writerow([course.id, course.course_name, course.teacher, course.start_date, course.start_time,
+                         course.number_of_lessons])
+
+    return response
+
+
+def csv_lessons_list_write(request):
+    """""Create a CSV file with teachers list"""
+    # Get all data from Teacher Database Table
+    lessons = Lesson.objects.all()
+
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="lessons_list.csv"'
+    response.write(u'\ufeff'.encode('utf8'))
+    writer = csv.writer(response, delimiter=';', dialect='excel')
+    writer.writerow(['Номер занятия в курсе', 'Название курса', 'Преподаватель', 'Тема занятия', 'Краткое описание',
+                     'Дата занятия', 'Время занятия', 'Комментарий'])
+
+    for lesson in lessons:
+        writer.writerow([lesson.number, lesson.course, lesson.teacher, lesson.topic, lesson.description,
+                         lesson.date, lesson.start_time, lesson.comment])
+
+    return response
