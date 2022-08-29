09be71aa3cd630d575d8e3b92daebe09a9f96ce2
Anton Petrunikov <apetrunikov@tutanota.com>
2022-08-29 19:30:09 +0300

    api location and comments updates

--- schedule_planner_be/api/comment/views.py
+++ schedule_planner_be/api/comment/views.py
@@ -1,27 +1,20 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from api.comment.serializers import CommentSerializer
 from schedule.models import Comment
+from api.location.permissions import LocationPermissionsMixin
+from rest_framework.permissions import IsAuthenticated
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_class = [permissions.IsAuthenticated]
 
-    def create(self, request, *args, **kwargs):
-        user_role = self.request.user.role
-        if user_role == 'Super Admin':
-            return True
-        raise PermissionError('User has not permission')
-
-    def update(self, request, *args, **kwargs):
-        user_role = self.request.user.role
-        if user_role == 'Super Admin':
-            return True
-        raise PermissionError('User has not permission')
-
-    def destroy(self, request, *args, **kwargs):
-        user_role = self.request.user.role
-        if user_role == 'Super Admin':
-            return True
-        raise PermissionError('User has not permission')
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'list':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAuthenticated & LocationPermissionsMixin]
+        return [permission() for permission in permission_classes]
