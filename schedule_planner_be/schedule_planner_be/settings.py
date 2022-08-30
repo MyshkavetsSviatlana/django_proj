7c6b130844e0570f1b65d3921b4c2b6405d6e6e6
Anton Petrunikov <apetrunikov@tutanota.com>
2022-08-30 09:37:59 +0300

    updated bootstrap

--- schedule_planner_be/schedule_planner_be/settings.py
+++ schedule_planner_be/schedule_planner_be/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
     'multiselectfield',
     'django_filters',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +120,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
