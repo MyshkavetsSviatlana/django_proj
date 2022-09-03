41e8d3caade3d3e73cd43e9c1c039658ecb0d6f4
Angelina Chernyavskaya <106554574+ag-chernyavskaya@users.noreply.github.com>
2022-09-03 22:17:19 +0300

    Update urls.py

--- schedule_planner_be/api/user/urls.py
+++ schedule_planner_be/api/user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.LoginAPIView.as_view(), name='login'),
+    path('login/', views.LoginAPIView.as_view(), name='login-api'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('email_verify/', views.VerifyEmail.as_view(), name='email-verify'),
 ]
