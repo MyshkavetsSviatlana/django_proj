42042e6ec2f1b380a258d48f0d96c4084a353aa9
Ангелина <a-hanevich@yandex.ru>
2022-09-05 13:29:48 +0300

    update settings

--- schedule_planner_be/schedule_planner_be/settings.py
+++ schedule_planner_be/schedule_planner_be/settings.py
@@ -157,7 +157,10 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+
     ),
 }
 
