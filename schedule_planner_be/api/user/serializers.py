9041c5a5529e7d8bca7b9b83a0af7fb1757a19c1
Ангелина <a-hanevich@yandex.ru>
2022-09-05 11:31:16 +0300

    token

--- schedule_planner_be/api/user/serializers.py
+++ schedule_planner_be/api/user/serializers.py
@@ -39,9 +39,16 @@ class EmailVerificationSerializer(serializers.ModelSerializer):
 
 
 class LoginSerializer(serializers.ModelSerializer):
-    tokens = serializers.CharField(max_length=555, read_only=True)
-    email = serializers.CharField(max_length=555)
-    password = serializers.CharField(max_length=555)
+    email = serializers.EmailField(max_length=150)
+    password = serializers.CharField(label='Password', validators=[
+        RegexValidator(
+            regex="^(?=.*[0-9].*)(?=.*[a-z].*)(?=.*[A-Z].*)[0-9a-zA-Z]{8}$",
+            message=_('Invalid password'),
+            code=_('invalid_password')
+        )], help_text=_('Password should have only 8 characters, '
+                        'at least one digit, one upper case letter and one lower case letter'),
+                                     write_only=True)
+    tokens = serializers.SerializerMethodField()
 
     def get_tokens(self, obj):
         user = User.objects.get(email=obj['email'])
@@ -52,17 +59,17 @@ class LoginSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'tokens')
+        fields = ['email', 'password', 'tokens']
 
     def validate(self, attrs):
         email = attrs.get('email', '')
         password = attrs.get('password', '')
         filtered_user_by_email = User.objects.filter(email=email)
         user = auth.authenticate(email=email, password=password)
-        if filtered_user_by_email.exists() and filtered_user_by_email[0].auth_provider != 'email':
-            raise AuthenticationFailed(
-                detail='Please continue your login using' + filtered_user_by_email[0].auth_provider
-            )
+        # if filtered_user_by_email.exists() and filtered_user_by_email[0].auth_provider != 'email':
+        #     raise AuthenticationFailed(
+        #         detail='Please continue your login using' + filtered_user_by_email[0].auth_provider
+        #     )
         if not user:
             raise AuthenticationFailed('Invalid credentials, try again')
         if not user.is_active:
--- schedule_planner_be/schedule_planner_be/settings.py
+++ schedule_planner_be/schedule_planner_be/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -155,9 +156,22 @@ REST_FRAMEWORK = {
     'DEFAULT_Filter_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    # ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFENITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    }
+}
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1)
+}
