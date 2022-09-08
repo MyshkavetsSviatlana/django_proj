ee5b687c3efc12990c7c739b89c02912db1e9bff
MyshkavetsSviatlana <myshkavets.sviatlana@gmail.com>
2022-09-08 10:43:14 +0300

    review classroomavaliability (delete foreignkey relation)

--- schedule_planner_be/schedule/admin.py
+++ schedule_planner_be/schedule/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Location, SubwayStation, Classroom, Schedule
 
-
 admin.site.register(Location)
 admin.site.register(SubwayStation)
 admin.site.register(Schedule)
