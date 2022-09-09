70923bb6ace9901069ea30f1ab2be9faf2179b73
Anton Petrunikov <apetrunikov@tutanota.com>
2022-09-09 10:27:57 +0300

    updated teacher app

--- schedule_planner_be/Teacher/forms.py
+++ schedule_planner_be/Teacher/forms.py
@@ -5,4 +5,4 @@ from .models import Teacher
 class TeacherForm(ModelForm):
     class Meta:
         model = Teacher
-        fields = '__all__'
+        fields = ['name', 'surname', 'fathers_name', 'specialization', 'course_name']
