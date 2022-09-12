65d8ebf8f2c8c8e565671cc1be7be68d809e8686
Angelina Chernyavskaya <106554574+ag-chernyavskaya@users.noreply.github.com>
2022-09-12 15:22:44 +0300

    Update urls.py

--- schedule_planner_be/api/lesson/urls.py
+++ schedule_planner_be/api/lesson/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.LessonListView.as_view()),
     path('morning/', views.LessonMorningListView.as_view()),
     path('evening/', views.LessonEveningListView.as_view()),
-    path('edit/<int:pk>/', views.LessonUpdateView.as_view()),
-    path('delete/<int:pk>/', views.LessonDeleteView.as_view()),
+    path('<int:pk>/edit/', views.LessonUpdateView.as_view()),
+    path('<int:pk>/delete/', views.LessonDeleteView.as_view()),
     path('<int:pk>/', views.LessonDetailView.as_view()),
     path('new/', views.LessonCreateView.as_view()),
     path('export/lessons-list/', views.csv_lessons_list_write, name='csv_lessons_list_write')
